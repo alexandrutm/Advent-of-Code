89f004a2f4b6b31804714f4e4956c635052745bb
AlexToma\Alexandru Toma <tomad.alexandru@gmail.com>
2022-12-04 07:43:10 +0200

    Solution for day 4

--- AdventOfCode/2022/day04.cpp
+++ AdventOfCode/2022/day04.cpp
@@ -1,1 +1,30 @@
+#include "../includes.h"
 
+void main()
+{
+  wifstream fin("data.txt");
+
+  int firstHalf  = 0;
+  int secondHalf = 0;
+
+  while (!fin.eof())
+  {
+    wchar_t com, under;
+    int     nr1, nr2, nr3, nr4;
+
+    fin >> nr1 >> under >> nr2 >> com >> nr3 >> under >> nr4;
+
+    if (nr1 <= nr3 && nr2 >= nr4)
+      firstHalf++;
+    else if (nr1 >= nr3 && nr2 <= nr4)
+      firstHalf++;
+
+    if (nr1 <= nr3 && nr2 >= nr3)
+      secondHalf++;
+    else if (nr1 >= nr3 && nr1 <= nr4)
+      secondHalf++;
+  }
+
+  cout << "First: " << firstHalf;
+  cout << "\nSecond: " << secondHalf;
+}
