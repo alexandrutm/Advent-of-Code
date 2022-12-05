dbc139e3add4b203b66430f9902ac89176a45913
AlexToma\Alexandru Toma <tomad.alexandru@gmail.com>
2022-12-05 23:35:11 +0200

    Solution for day 1

---
+++ AdventOfCode/2019/day01.cpp
@@ -0,0 +1,35 @@
+#include "../lib.h"
+
+void day1()
+{
+  ifstream fin("data.txt");
+  string   line;
+  int      sumP1 = 0;
+  int      sumP2 = 0;
+
+  while (!fin.eof())
+  {
+    int line;
+    fin >> line;
+
+    int firstSum = (floor(line / 3)) - 2;
+
+    sumP1 += firstSum;
+    sumP2 += firstSum;
+
+    while (firstSum > 9)
+    {
+      int intermediarSum = (floor(firstSum / 3)) - 2;
+      sumP2 += intermediarSum;
+      firstSum = intermediarSum;
+    }
+  }
+
+  cout << "First half: " << sumP1;
+  cout << "\nSecond half: " << sumP2;
+}
+
+void main()
+{
+  day1();
+}
