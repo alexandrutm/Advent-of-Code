cebf24f8a05d6dcdef85539dfc2a83285de0b215
AlexToma\Alexandru Toma <tomad.alexandru@gmail.com>
2022-12-12 19:43:04 +0200

    Second half solution

--- AdventOfCode/2022/day09.cpp
+++ AdventOfCode/2022/day09.cpp
@@ -8,78 +8,22 @@ struct point
   bool operator<(const point & p) const { return x < p.x && y < p.y; }
 };
 
-int ManhDist(point aHead, point aTail)
+void move_tail(point & aHead, point & aTail)
 {
-  return (abs(aHead.x - aTail.x) + abs(aHead.y - aTail.y));
-}
+  int x = aHead.x - aTail.x;
+  int y = aHead.y - aTail.y;
 
-bool IsNeighbour(point aHead, point aTail)
-{
-  for (int i = -1; i < 2; i++)
+  if (abs(x) > 1 || abs(y) > 1)
   {
-    for (int j = -1; j < 2; j++)
+    if (x == 0)
+      aTail.y += y / 2;
+    else if (y == 0)
+      aTail.x += x / 2;
+    else
     {
-      if (aTail.x + i == aHead.x && aTail.y + j == aHead.y)
-      {
-        return true;
-      }
+      aTail.x += (x > 0) ? 1 : -1;
+      aTail.y += (y > 0) ? 1 : -1;
     }
-  }
-
-  return false;
-}
-
-void move_tail(vector<point> & aRope, const char aDirection)
-{
-  if (ManhDist(aRope.front(), aRope.back()) == 2 && IsNeighbour(aRope.front(), aRope.back()))
-    return;
-
-  if (ManhDist(aRope.front(), aRope.back()) >= 2)
-  {
-    if (aDirection == 'U')
-    {
-      aRope.back().x = aRope.front().x + 1;
-      aRope.back().y = aRope.front().y;
-    }
-    else if (aDirection == 'R')
-    {
-      aRope.back().y = aRope.front().y - 1;
-      aRope.back().x = aRope.front().x;
-    }
-    else if (aDirection == 'D')
-    {
-      aRope.back().x = aRope.front().x - 1;
-      aRope.back().y = aRope.front().y;
-    }
-    else if (aDirection == 'L')
-    {
-      aRope.back().y = aRope.front().y + 1;
-      aRope.back().x = aRope.front().x;
-    }
-  }
-}
-
-void move_rope(vector<point> & aRope, const char aDirection)
-{
-  if (aDirection == 'U')
-  {
-    aRope.front().x -= 1;
-    move_tail(aRope, aDirection);
-  }
-  else if (aDirection == 'R')
-  {
-    aRope.front().y += 1;
-    move_tail(aRope, aDirection);
-  }
-  else if (aDirection == 'D')
-  {
-    aRope.front().x += 1;
-    move_tail(aRope, aDirection);
-  }
-  else if (aDirection == 'L')
-  {
-    aRope.front().y -= 1;
-    move_tail(aRope, aDirection);
   }
 }
 
@@ -92,10 +36,19 @@ void day09()
 
   vector<point> rope;
 
-  point start, tail, head;
+  point head;
 
-  rope.push_back(head);
-  rope.push_back(tail);
+  // p1
+  // rope.push_back(head);
+  // rope.push_back(head);
+
+  // p2
+  int i = 0;
+  while (i < 10)
+  {
+    rope.push_back(head);
+    i++;
+  }
 
   char direction;
   int  steps;
@@ -104,15 +57,21 @@ void day09()
   {
     for (int i = 0; i < steps; ++i)
     {
-      move_rope(rope, direction);
+      int dx = direction == 'R' ? 1 : (direction == 'L' ? -1 : 0);
+      int dy = direction == 'U' ? 1 : (direction == 'D' ? -1 : 0);
+
+      rope.front().x += dx;
+      rope.front().y += dy;
+
+      // p1
+      // move_tail(rope.front(), rope.back());
+
+      for (int i = 0; i < rope.size() - 1; i++)
+        move_tail(rope[i], rope[i + 1]);
+
       visited.insert(make_pair(rope.back().x, rope.back().y));
     }
   }
 
   cout << visited.size();
-}
-
-void main()
-{
-  day09();
 }
